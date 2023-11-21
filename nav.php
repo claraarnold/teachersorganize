cea3acbc2b55e9a2a49f0fd9e26daffd7bdafdc1
Clara Arnold <claraarnold@u.boisestate.edu>
2023-11-21 12:47:22 -0700

    Adding highlighted nav bar

--- nav.php
+++ nav.php
@@ -9,12 +9,12 @@
         <div class="colored-box">
             <div class="nav-bar">
                 <ul>
-                    <li><a href="homebase.php">Homebase</a></li>
-                    <li><a href="images.php">Images</a></li>
-                    <li><a href="videos.php">Videos</a></li>
-                    <li><a href="lessonplan.php">Lesson-Plans</a></li>
-                    <li><a href="blogs.php">Blogs</a></li>
-                    <li><a href="resources.php">Resources</a></li>
+                    <li <?php if (basename($_SERVER['PHP_SELF']) == 'homebase.php') echo 'class="active"'; ?>><a href="homebase.php">Homebase</a></li>
+                    <li <?php if (basename($_SERVER['PHP_SELF']) == 'images.php') echo 'class="active"'; ?>><a href="images.php">Images</a></li>
+                    <li <?php if (basename($_SERVER['PHP_SELF']) == 'videos.php') echo 'class="active"'; ?>><a href="videos.php">Videos</a></li>
+                    <li <?php if (basename($_SERVER['PHP_SELF']) == 'lessonplan.php') echo 'class="active"'; ?>><a href="lessonplan.php">Lesson-Plans</a></li>
+                    <li <?php if (basename($_SERVER['PHP_SELF']) == 'blogs.php') echo 'class="active"'; ?>><a href="blogs.php">Blogs</a></li>
+                    <li <?php if (basename($_SERVER['PHP_SELF']) == 'resources.php') echo 'class="active"'; ?>><a href="resources.php">Resources</a></li>
                     <li><a href="logout.php">Logout</a></li>
                 </ul>
             </div>
